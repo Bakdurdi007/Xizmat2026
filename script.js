1517c182d1d17e36025288b1b90e0fd4993d4200
bakdurdi-max <68939725+bakdurdi-max@users.noreply.github.com>
2026-01-09 11:56:53 +0500

    O'zgarish

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const cardContainer = document.getElementById('card-container');
 
 // 10 ta xizmat ma'lumotlari
 const cardData = [
-    { title: "Dasturlash", desc: "Veb va mobil ilovalar yaratish xizmati.", img: "valve.png" },
-    { title: "Grafik Dizayn", desc: "Brending va logotip yaratish.", img: "electrical-energy.png" },
-    { title: "Marketing", desc: "Ijtimoiy tarmoqlarda reklama yuritish.", img: "gas-cylinder.png" },
+    { title: "Tabiiy gaz", desc: "Veb va mobil ilovalar yaratish xizmati.", img: "valve.png" },
+    { title: "Elektroenergiya", desc: "Brending va logotip yaratish.", img: "electrical-energy.png" },
+    { title: "Suyultirilgan gaz", desc: "Ijtimoiy tarmoqlarda reklama yuritish.", img: "gas-cylinder.png" },
     { title: "Kiberxavfsizlik", desc: "Tizimlarni tashqi hujumlardan himoya qilish.", img: "policeman.png" },
-    { title: "SMM", desc: "Targeting va kreativ kontent yaratish.", img: "surgery-room.png" },
-    { title: "Data Science", desc: "Ma'lumotlarni tahlil qilish va prognozlash.", img: "truck.png" }
+    { title: "Tibbiy yordam", desc: "Targeting va kreativ kontent yaratish.", img: "surgery-room.png" },
+    { title: "Chiqindilarni olib ketish", desc: "Ma'lumotlarni tahlil qilish va prognozlash.", img: "truck.png" }
 ];
 
 // Cardlarni yaratish funksiyasi
